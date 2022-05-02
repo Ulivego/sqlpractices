a8061211b16a8cf65e3120582053980e1ea63cc9
Ulises <71343501+Ulivego@users.noreply.github.com>
2022-05-02 09:08:40 -0500

    Create practice9.sql

---
+++ practice9.sql
@@ -0,0 +1,160 @@
+CREATE TABLE us_counties_pop_est_2019 (
+    state_fips text,                         -- State FIPS code
+    county_fips text,                        -- County FIPS code
+    region smallint,                         -- Region
+    state_name text,                         -- State name	
+    county_name text,                        -- County name
+    area_land bigint,                        -- Area (Land) in square meters
+    area_water bigint,                       -- Area (Water) in square meters
+    internal_point_lat numeric(10,7),        -- Internal point (latitude)
+    internal_point_lon numeric(10,7),        -- Internal point (longitude)
+    pop_est_2018 integer,                    -- 2018-07-01 resident total population estimate
+    pop_est_2019 integer,                    -- 2019-07-01 resident total population estimate
+    births_2019 integer,                     -- Births from 2018-07-01 to 2019-06-30
+    deaths_2019 integer,                     -- Deaths from 2018-07-01 to 2019-06-30
+    international_migr_2019 integer,         -- Net international migration from 2018-07-01 to 2019-06-30
+    domestic_migr_2019 integer,              -- Net domestic migration from 2018-07-01 to 2019-06-30
+    residual_2019 integer,                   -- Residual for 2018-07-01 to 2019-06-30
+    CONSTRAINT counties_2019_key PRIMARY KEY (state_fips, county_fips)	
+);
+
+-- Listing 13-1: Using a subquery in a WHERE clause
+SELECT county_name,
+       state_name,
+       pop_est_2019
+FROM us_counties_pop_est_2019
+WHERE pop_est_2019 >= (
+    SELECT percentile_cont(.9) WITHIN GROUP (ORDER BY pop_est_2019)
+    FROM us_counties_pop_est_2019
+    )
+ORDER BY pop_est_2019 DESC;
+
+-- Listing 13-3: Subquery as a derived table in a FROM clause
+SELECT round(calcs.average, 0) as average,
+       calcs.median,
+       round(calcs.average - calcs.median, 0) AS median_average_diff
+FROM (
+     SELECT avg(pop_est_2019) AS average,
+            percentile_cont(.5)
+                WITHIN GROUP (ORDER BY pop_est_2019)::numeric AS median
+     FROM us_counties_pop_est_2019
+     )
+AS calcs;
+
+-- Listing 13-4: Joining two derived tables
+SELECT census.state_name AS st,
+       census.pop_est_2018,
+       est.establishment_count,
+       round((est.establishment_count/census.pop_est_2018::numeric) * 1000, 1)
+           AS estabs_per_thousand
+FROM
+    (
+         SELECT st,
+                sum(establishments) AS establishment_count
+         FROM cbp_naics_72_establishments
+         GROUP BY st
+    )
+    AS est
+JOIN
+    (
+        SELECT state_name,
+               sum(pop_est_2018) AS pop_est_2018
+        FROM us_counties_pop_est_2019
+        GROUP BY state_name
+    )
+    AS census
+ON est.st = census.state_name
+ORDER BY estabs_per_thousand DESC;
+
+-- Listing 13-5: Adding a subquery to a column list
+SELECT county_name,
+       state_name AS st,
+       pop_est_2019,
+       (SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY pop_est_2019)
+        FROM us_counties_pop_est_2019) AS us_median
+FROM us_counties_pop_est_2019;
+
+-- Listing 13-6: Using a subquery in a calculation
+SELECT county_name,
+       state_name AS st,
+       pop_est_2019,
+       pop_est_2019 - (SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY pop_est_2019)
+                       FROM us_counties_pop_est_2019) AS diff_from_median
+FROM us_counties_pop_est_2019
+WHERE (pop_est_2019 - (SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY pop_est_2019)
+                       FROM us_counties_pop_est_2019))
+       BETWEEN -1000 AND 1000;
+	   
+-- Listing 13-7: Creating and filling a retirees table
+CREATE TABLE retirees (
+    id int,
+    first_name text,
+    last_name text
+);
+
+INSERT INTO retirees
+VALUES (2, 'Janet', 'King'),
+       (4, 'Michael', 'Taylor');
+	   
+-- Listing 13-8: Generating values for the IN operator
+SELECT first_name, last_name
+FROM employees
+WHERE emp_id IN (
+    SELECT id
+    FROM retirees)
+ORDER BY emp_id;
+
+-- Listing 13-9: Using a correlated subquery with WHERE EXISTS
+SELECT first_name, last_name
+FROM employees
+WHERE EXISTS (
+    SELECT id
+    FROM retirees
+    WHERE id = employees.emp_id);
+	
+-- Listing 13-11: Using LATERAL subqueries in the FROM clause
+SELECT county_name,
+       state_name,
+       pop_est_2018,
+       pop_est_2019,
+       raw_chg,
+       round(pct_chg * 100, 2) AS pct_chg
+FROM us_counties_pop_est_2019,
+     LATERAL (SELECT pop_est_2019 - pop_est_2018 AS raw_chg) rc,
+     LATERAL (SELECT raw_chg / pop_est_2018::numeric AS pct_chg) pc
+ORDER BY pct_chg DESC;
+
+-- Common Table Expressions
+-- Listing 13-13: Using a simple CTE to count large counties
+WITH large_counties (county_name, state_name, pop_est_2019)
+AS (
+    SELECT county_name, state_name, pop_est_2019
+    FROM us_counties_pop_est_2019
+    WHERE pop_est_2019 >= 100000
+   )
+SELECT state_name, count(*)
+FROM large_counties
+GROUP BY state_name
+ORDER BY count(*) DESC;
+
+-- Listing 13-14: Using CTEs in a table join
+WITH
+    counties (st, pop_est_2018) AS
+    (SELECT state_name, sum(pop_est_2018)
+     FROM us_counties_pop_est_2019
+     GROUP BY state_name),
+
+    establishments (st, establishment_count) AS
+    (SELECT st, sum(establishments) AS establishment_count
+     FROM cbp_naics_72_establishments
+     GROUP BY st)
+
+SELECT counties.st,
+       pop_est_2018,
+       establishment_count,
+       round((establishments.establishment_count / 
+              counties.pop_est_2018::numeric(10,1)) * 1000, 1)
+           AS estabs_per_thousand
+FROM counties JOIN establishments
+ON counties.st = establishments.st
+ORDER BY estabs_per_thousand DESC;
